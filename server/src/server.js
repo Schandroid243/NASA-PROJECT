879de0732dddd36985cc0d0afe10ddc282f14c50
Schandroid243 <schadrackngunza@gmail.com>
2025-07-17 15:01:13 +0100

    first comit

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const app = require("./app");
+
+const PORT = process.env || 8000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is listenning on port ${PORT}`);
+});
