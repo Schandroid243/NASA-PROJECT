3b4063a85a33e2bd2fb279d50212404a07dbd573
Schandroid243 <schadrackngunza@gmail.com>
2025-07-17 18:34:18 +0100

    feat(server/src/models/planets.model.js): Added a stream to convert a csv file into a javascript array of objects in order to retrieve the habitable planets only based on the csv file.
    Added a function which return a promise that will block the code execution till the event emitter of the stream ends and then resolve the promise.
    
    feat(server/src/server.js):  Import the loadPlanetData function which return a promise, and after creating the server, we add an instruction to await for the promise to be resolved before the server start listenning.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,57 @@
-const planets = [];
+const { rejects } = require("assert");
+const { error } = require("console");
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const { resolve } = require("path");
 
-module.exports = planets;
+let habitablePlanets = [];
+
+function isHabitablePlanets(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+/*
+ const promise = new Promise((resolve, reject) => {
+    resolve(42);
+ });
+ promise.then((results) => {
+
+});
+const results = await promise;
+console.log(results);
+*/
+function loadPlanetsData() {
+  return new Promise((resolve, rejects) => {
+    fs.createReadStream("kepler_data.csv")
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanets(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        rejects(err);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} habitable planets found !`);
+        resolve();
+        console.log("We are don processing our file !");
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
